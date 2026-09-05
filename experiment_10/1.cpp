ea802ebf2fbb6d1368e0f2a5b39e92a727af8b80
shinzo <88925036+Shingura@users.noreply.github.com>
2026-09-05 15:50:58 +0800

    rewrite experiment_10/1.cpp in modern cpp

--- experiment_10/1.cpp
+++ experiment_10/1.cpp
@@ -26,3 +26,31 @@ int main()
     }
     cout << "数组中没有该元素！" << endl;
 }
+
+/* 现代 C++ 实现：
+#include <algorithm>
+#include <iostream>
+#include <array>
+#include <iterator>
+
+int main()
+{
+    std::array<int, 12> a = {2, 4, 6, 8, 10, 12, 
+                            14, 16, 18, 20, 22, 24}; // 共 12 个数
+    int num { };
+    std::cin >> num;
+
+    auto it = std::find(a.begin(), a.end(), num);
+
+    if ( it != a.end() )
+    {
+        std::cout << "该数在数组中的序号为" << std::distance(a.begin(),it) << '\n';
+    }
+    else {
+        std::cout << "数组中没有该元素！\n" ;
+    }
+
+    return 0;
+}
+*/
+
